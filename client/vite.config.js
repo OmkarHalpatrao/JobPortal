6716a663024eea5ceb3f95a467482bc687e1eeb8
Omkar <omkarhalpatrao@gmail.com>
2025-04-25 13:47:33 +0530

    fix tailwind setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [
     react({
       jsxRuntime: 'automatic' // ⬅️ Ensure it's using new JSX transform
-    })
+    }),tailwindcss()
   ],
   resolve: {
     alias: {
